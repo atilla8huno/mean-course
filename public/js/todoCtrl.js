11f662ae9bdf7f41c49f8b896341d584111e5775
Atilla Barros <atilla8huno@gmail.com>
2016-04-05 17:12:05 -0300

    adicionando listagem de todos na tela

--- public/js/todoCtrl.js
+++ public/js/todoCtrl.js
@@ -3,9 +3,20 @@
     
     angular.module('TodoApp').controller('TodoCtrl', TodoCtrl);
     
-    function TodoCtrl($scope) {
+    function TodoCtrl($scope, $http) {
         
         $scope.name = 'Átilla';
+        $scope.todos = [];
         
+        $scope.listar = function () {
+            $http
+                .get('/todo-app/api/todos/atilla')
+                .then(function (result) {
+                    $scope.todos = result.data;
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        }
     }
 })();
