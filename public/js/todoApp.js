49fa4c7e8d370cadb1d0eff7655582acc7a2dd34
Átilla Barros <tilla Barros>
2016-04-16 09:53:51 -0300

    facilitando entendimento da configuracao das rotas

--- public/js/todoApp.js
+++ public/js/todoApp.js
@@ -1,29 +1,36 @@
 (function() {
     'use strict';
 
+    // definicao do modulo do app
     angular.module('TodoApp', ['ngRoute', 'angular-ladda']);
 
-    angular.module('TodoApp').config(['$routeProvider',
-        function($routeProvider) {
-            $routeProvider.
-                when('/todos/:usuario', {
-                    templateUrl: 'pesquisa.html',
-                    controller: 'TodoPesquisaCtrl'
-                }).
-                when('/todos', {
-                    templateUrl: 'pesquisa.html',
-                    controller: 'TodoPesquisaCtrl'
-                }).
-                when('/todo/:id', {
-                    templateUrl: 'cadastro.html',
-                    controller: 'TodoCadastroCtrl'
-                }).
-                when('/todo', {
-                    templateUrl: 'cadastro.html',
-                    controller: 'TodoCadastroCtrl'
-                }).
-                otherwise({
-                    redirectTo: '/todos'
-                });
-        }]);
+    // usa o modulo definido e configura as rotas
+    angular.module('TodoApp').config(Config);
+    
+    // faz injecao do service $routeProvider na function Config 
+    Config.$inject = ['$routeProvider'];
+    
+    // define as rotas
+    function Config($routeProvider) {
+        $routeProvider.
+            when('/todos/:usuario', {
+                templateUrl: 'pesquisa.html',
+                controller: 'TodoPesquisaCtrl'
+            }).
+            when('/todos', {
+                templateUrl: 'pesquisa.html',
+                controller: 'TodoPesquisaCtrl'
+            }).
+            when('/todo/:id', {
+                templateUrl: 'cadastro.html',
+                controller: 'TodoCadastroCtrl'
+            }).
+            when('/todo', {
+                templateUrl: 'cadastro.html',
+                controller: 'TodoCadastroCtrl'
+            }).
+            otherwise({
+                redirectTo: '/todos'
+            });
+    }
 })();
